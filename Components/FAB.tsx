5d9050b47f8422681577b8e9bef846a8f3d0e2fc
Memo Cachú <gcachu.o@gmail.com>
2022-11-27 15:11:02 -0600

    Credit card limit

---
+++ Components/FAB.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { FAB } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+
+function FABButton() {
+  const navigation = useNavigation() as DrawerNavigationProp<any>;
+  return (
+    <FAB
+      icon="plus"
+      style={styles.fab}
+      onPress={() => navigation.navigate("Presupuesto")}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+export default FABButton;
--- Screen/Cuentas/index.tsx
+++ Screen/Cuentas/index.tsx
@@ -1,10 +1,12 @@
 import { RefreshControl, ScrollView } from "react-native";
 import { useCallback, useState } from "react";
-import { BottomNavigation, Card, Title } from "react-native-paper";
+import { BottomNavigation, Card, Paragraph, Title } from "react-native-paper";
 import numeral from "numeral";
+import creditCardsJSON from "../../creditcards.env.json";
 
 import BudgetsAPI, { IAccounts } from "../../API/Budgets";
 import { useFocusEffect } from "@react-navigation/native";
+import FABButton from "../../Components/FAB";
 
 export default function Cuentas() {
   const [index, setIndex] = useState(0);
@@ -39,10 +41,14 @@ export default function Cuentas() {
 function AccountList(props: { onBudget: boolean }) {
   const [refresh, setRefresh] = useState(false);
   const [accounts, setAccounts] = useState([] as IAccounts[]);
+  const [creditCards, setCreditCards] = useState(
+    {} as { [index: string]: { limit: number; name: string } }
+  );
 
   useFocusEffect(
     useCallback(() => {
       onRefresh();
+      setCreditCards(creditCardsJSON);
     }, [])
   );
 
@@ -76,41 +82,56 @@ function AccountList(props: { onBudget: boolean }) {
   }
 
   return (
-    <ScrollView
-      style={{ paddingHorizontal: 20 }}
-      refreshControl={
-        <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
-      }
-    >
-      {!accounts.length ? (
-        <></>
-      ) : (
-        accounts.map((account) => (
-          <Card key={account.id} style={{ marginVertical: 10 }}>
-            <Card.Title
-              title={account.name}
-              subtitle={
-                {
-                  checking: "Débito",
-                  savings: "Ahorro",
-                  cash: "Efectivo",
-                  creditCard: "Crédito",
-                  otherAsset: "Otro",
-                  personalLoan: "Préstamo",
-                  mortgage: "Hipoteca",
-                }[account.type] ?? account.type
-              }
-            />
-            <Card.Content>
-              <Title
-                style={{ color: account.balance > 0 ? "#009100" : "#d00000" }}
-              >
-                {numeral(account.balance / 1000).format("$#,#.##")}
-              </Title>
-            </Card.Content>
-          </Card>
-        ))
-      )}
-    </ScrollView>
+    <>
+      <ScrollView
+        style={{ paddingHorizontal: 20 }}
+        refreshControl={
+          <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+        }
+      >
+        {!accounts.length ? (
+          <></>
+        ) : (
+          accounts.map((account) => {
+            const accountType =
+              {
+                checking: "Débito",
+                savings: "Ahorro",
+                cash: "Efectivo",
+                creditCard: "Crédito",
+                otherAsset: "Otro",
+                personalLoan: "Préstamo",
+                mortgage: "Hipoteca",
+              }[account.type] ?? account.type;
+            const balance = account.balance / 1000;
+
+            return (
+              <Card key={account.id} style={{ marginVertical: 10 }}>
+                <Card.Title title={account.name} subtitle={accountType} />
+                <Card.Content>
+                  <Title
+                    style={{
+                      color: account.balance > 0 ? "#009100" : "#d00000",
+                    }}
+                  >
+                    {numeral(balance).format("$#,#.##")}
+                  </Title>
+                  {account.type === "creditCard" && (
+                    <>
+                      <Paragraph>
+                        {numeral(
+                          creditCards[account.id].limit + balance
+                        ).format("$#,#.##")}
+                      </Paragraph>
+                    </>
+                  )}
+                </Card.Content>
+              </Card>
+            );
+          })
+        )}
+      </ScrollView>
+      <FABButton />
+    </>
   );
 }
